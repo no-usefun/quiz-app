02ea57d0179cfbd022a89b1944742544391083a7
Harsh Agarwal <itsharsh636@gmail.com>
2026-08-14 22:46:35 +0530

    2 baar

--- src/main/java/com/quiz_app/backend/security/SecurityConfig.java
+++ src/main/java/com/quiz_app/backend/security/SecurityConfig.java
@@ -12,8 +12,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(
             HttpSecurity http) throws Exception {
 
-        http
-                .csrf(csrf -> csrf.disable())
+        http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll());
 
