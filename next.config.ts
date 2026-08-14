8e46efcde8d07daed4b877447a40d3d9990f23f4
Surysnahu Saini <suryanshusaini2009@gmail.com>
2026-08-14 21:39:36 +0530

    chore: update configs, dependencies, and remove Next.js boilerplate assets

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Add the IP address from your error message here
+  devIndicators: false,
   allowedDevOrigins: ["172.20.32.53", "localhost:3000"],
-
-  // Keep any other existing configuration you might have below...
 };
 
 export default nextConfig;
+
