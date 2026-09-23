9343f6a2ac11092bdc8de796683c8b50954de3b7
Harsh Agarwal <itsharsh636@gmail.com>
2026-09-24 02:52:57 +0530

    removed the old legacy auth system with the new auth system

--- backend/src/main/java/com/quiz_app/backend/dto/user/CreateTeacherRequest.java
+++
@@ -1,9 +0,0 @@
-package com.quiz_app.backend.dto.user;
-
-public record CreateTeacherRequest(
-                String firstName,
-                String lastName,
-                String email,
-                String password,
-                String registrationNo) {
-}
